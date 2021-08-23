42b850af6eceb1eb93b91550e9ef8e2924f98842
Jose <jsd.matt@gmail.com>
2021-08-23 17:34:24 -0300

    intento de registro

--- JuegosChaco/apps/usuario/views.py
+++ JuegosChaco/apps/usuario/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import Usuario
 
 @login_required
 def Perfil(request):
     return render(request, 'usuario/perfil.html')
+
+
+class RegistroUsuario(CreateView):
+    model = Usuario
+    template_name = 'usuario/registrar.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    
